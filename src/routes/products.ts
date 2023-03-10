afa56cd566d1414a5a44b370c4ed347f09b9822e
Andreas Xu <junhaoxu123@gmail.com>
2023-03-10 10:35:32 +0100

    Changed to correct product_id + validators

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -5,7 +5,7 @@ const router = express.Router()
 
 router.get('/', index)
 
-router.get('/:productId', show)
+router.get('/:product_id', show)
 
 router.post('/', [
     body('name')
@@ -14,6 +14,21 @@ router.post('/', [
 
     body('price')
     .isInt({min: 1}).withMessage("Add a Price"),
+
+    body('description')
+    .isLength({min: 1}).bail()
+    .isString(),
+
+    body('images')
+    .isObject(),
+
+    body('stock_status')
+    .isLength({min: 1}).bail()
+    .isString(),
+
+    body('stock_quantity')
+    .isInt()
+
 
 ], store)
 
